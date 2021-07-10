12fd600d7dbba95906273f45d1d4ee6eba3f591c
syugokawasaki <n.m.raminas@gmail.com>
2021-07-10 08:04:19 +0000

    [Finish] 3-7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -14,7 +14,7 @@ class BlogsController < ApplicationController
   def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to blogs_path(blog,id)
+    redirect_to blogs_path
 
   end
 
@@ -29,6 +29,9 @@ class BlogsController < ApplicationController
   end
 
   def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
   end
 
   private
